c735fc1957d4eaf0a7cdb5b786f00f9aaa61f848
Kawashima Daichi <daichi0.0daichi@gmail.com>
2014-09-28 17:17:35 +0900

    Initial Commit

---
+++ DigitalSax2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DigitalSax2
+//
+//  Created by 川島 大地 on 2014/09/28.
+//  Copyright (c) 2014年 川島 大地. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DigitalSax2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DigitalSax2
+//
+//  Created by 川島 大地 on 2014/09/28.
+//  Copyright (c) 2014年 川島 大地. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
